17e7fa9edb14743c06b1eaebc2a42b8230ca9783
Костенко Вадим <kostenko@mdrk.ru>
2022-08-18 16:08:53 +0700

    3rd review

--- script/script.js
+++ script/script.js
@@ -6,20 +6,23 @@ let subtitle = document.querySelector('.profile__subtitle');
 let popupSubtitle = document.querySelector("input[name='subtitle']");
 let saveButton = document.querySelector('.popup__button');
 let close = document.querySelector('.popup__close-icon');
+let formElement = document.querySelector('.popup__form');
 
 function editProfile() {
     popupTitle.value = title.textContent;
     popupSubtitle.value = subtitle.textContent;
     popup.classList.add('popup_opened');
 };
-let formElement = document.querySelector('.popup__form');
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     title.textContent = popupTitle.value;
     subtitle.textContent = popupSubtitle.value;
     popup.classList.remove('popup_opened');
 }
+function closePopup () {
+    popup.classList.remove('popup_opened');
+}
 
 button.addEventListener('click', editProfile);
-close.addEventListener('click',popup.classList.remove('popup_opened'));
-formElement.addEventListener('submit', formSubmitHandler); 
+close.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
