13683b4b1260d23dc79817ce4ab33521df647608
Костенко Вадим <kostenko@mdrk.ru>
2022-09-03 11:11:03 +0700

    1st review

--- script/script.js
+++ script/script.js
@@ -1,11 +1,12 @@
 const profileEditButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
 const popup = document.querySelector('.popup');
+const popupElementsArray = document.querySelectorAll('.popup');
 const title = document.querySelector('.profile__title');
 const popupTitle = document.querySelector("input[name='title']");
 const subtitle = document.querySelector('.profile__subtitle');
 const popupSubtitleInput = document.querySelector("input[name='subtitle']");
-const close = document.querySelector('.popup__close-button');
+const closeButton = document.querySelectorAll('.popup__close-button');
 const closeNewItemButton = document.querySelector('.new-item__close-button');
 const formElement = document.querySelector('.popup__form');
 const listElement = document.querySelector('.elements__list');
@@ -50,10 +51,10 @@ function openEditProfilePopup() {
   popup.classList.add('popup_opened');
 }
 function addNewItemPopup() {
-  newItemElement.classList.add('new-item_opened');
+  newItemElement.classList.add('popup_opened');
 }
 function closeNewItem() {
-  newItemElement.classList.remove('new-item_opened');
+  newItemElement.classList.remove('popup_opened');
 }
 function formProfileSubmitHandler (evt) {
   evt.preventDefault(); 
@@ -62,24 +63,30 @@ function formProfileSubmitHandler (evt) {
   popup.classList.remove('popup_opened');
 }
 
-function closePopup () {
-    newItemElement.classList.remove('new-item_opened');
-    popup.classList.remove('popup_opened');
+function closePopup (el) {
+    //newItemElement.classList.remove('popup_opened');
+  el.classList.remove('popup_opened');
 }
 function submitButtonHandler(evt) {
   evt.preventDefault(); 
   addCard({place: placeInput.value,link: urlInput.value});
-  newItemElement.classList.remove('new-item_opened');
+  newItemElement.classList.remove('popup_opened');
 }
 function closeFullViewImg () {
   imgPopupElement.classList.remove('image-popup_opened');
+}
+function closeButtonHandler (item) {
+  const closeButEl= item.querySelector('.popup__close-button');
+  closeButEl.addEventListener('click', closePopup(item));
+  console.log(closeButEl);
 }
 
 initialCards.forEach(addCard);
 profileEditButton.addEventListener('click', openEditProfilePopup);
 addButton.addEventListener('click', addNewItemPopup);
-close.addEventListener('click', closePopup);
-closeNewItemButton.addEventListener('click',closeNewItem);
+popupElementsArray.forEach(closeButtonHandler);
+//close.addEventListener('click', closePopup);
+//closeNewItemButton.addEventListener('click',closeNewItem);
 formElement.addEventListener('submit',formProfileSubmitHandler);
 submitFormAddElement.addEventListener('submit',submitButtonHandler);
 closeFullvieweImageBtn.addEventListener('click',closeFullViewImg);
